e18029ab6ba133a3c6d2df1f8de31aa2264581b2
timabs <timur.abd.nyc@gmail.com>
2024-05-10 14:07:52 -0400

    set up guessing JSX + form handling

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Home } from "./Components/Home";
 
 const App: FC = () => {
   return (
-    <div className="h-screen w-screen font-protest">
+    <div className="h-screen w-screen font-rethink">
       <Home />
     </div>
   );
--- src/Components/Hangman.tsx
+++ src/Components/Hangman.tsx
@@ -3,7 +3,10 @@ import { getRandomWord } from "../API/GetWord";
 
 export const Hangman: FC = () => {
   const [randomWord, setRandomWord] = useState<string[]>();
+  const [guess, setGuess] = useState<string>();
+  const [gameStarted, setGameStart] = useState<boolean>();
   const retrieveRandomWord = async () => {
+    setGameStart(true);
     let randomWord: string = await getRandomWord();
     const wordLength = randomWord.length;
     if (wordLength < 4 || wordLength > 20) {
@@ -13,19 +16,55 @@ export const Hangman: FC = () => {
       setRandomWord([...randomWord, randomWord[i]]);
     }
   };
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setGuess(e.target.value);
+  };
+  const handleGuess = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(guess);
+  };
   return (
     <div className="flex items-center justify-center flex-col gap-6">
       <button
         onClick={() => retrieveRandomWord()}
-        className="font-rethink text-white bg-green-700 px-4 py-2 rounded-md hover:bg-green-800"
+        className={`${
+          gameStarted ? "hidden" : ""
+        } font-rethink text-white bg-green-700 px-4 py-2 rounded-md hover:bg-green-800`}
       >
         Start
       </button>
       <div className="flex flex-row gap-6">
-        {randomWord?.map(() => (
-          <div className="text-5xl">__</div>
+        {randomWord?.map((letter, index) => (
+          <div className="text-5xl" data-letterpos={index + 1} key={index}>
+            __
+          </div>
         ))}
       </div>
+      <form
+        onSubmit={(e) => handleGuess(e)}
+        className={`${
+          gameStarted ? "" : "hidden"
+        } flex flex-col gap-3 justify-center items-center`}
+      >
+        <label
+          htmlFor="guess"
+          className="font-rethink flex flex-row gap-3 items-center justify-center"
+        >
+          <input
+            type="text"
+            id="guess"
+            name="guess"
+            value={guess}
+            onChange={(e) => handleChange(e)}
+            className="outline-none border-b-2 w-2/3 border-green-700 active:outline-1 px-2 py-1"
+          ></input>
+        </label>
+        <input
+          type="submit"
+          value="Guess"
+          className="bg-green-700 text-white rounded-md px-4 py-2 w-2/3 hover:cursor-pointer hover:bg-green-800"
+        ></input>
+      </form>
     </div>
   );
 };
